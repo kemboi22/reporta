92851f57418e92b34a808071547c91da3b886c3c
Kemboi Elvis <kemboielvis22@gmail.com>
2025-12-27 00:57:25 +0300

    feat: added prisma

---
+++ server/utils/auth.ts
@@ -0,0 +1,29 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import {
+  admin,
+  emailOTP,
+  phoneNumber,
+  twoFactor,
+  username,
+} from "better-auth/plugins";
+import { passkey } from "@better-auth/passkey";
+import { prisma } from "./db";
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [
+    twoFactor(),
+    username(),
+    phoneNumber(),
+    admin(),
+    passkey(),
+    // emailOTP({
+    //   sendVerificationOTP(data) {return data},
+    // }),
+  ],
+});
---
+++ server/utils/db.ts
@@ -0,0 +1,15 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@/generated/prisma/client";
+
+const prismaClientSingleton = () => {
+  const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+  return new PrismaClient({ adapter: pool });
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
